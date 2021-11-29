b3af56c1a448b4ff34c90a466572ed4dee26ea52
Hisyam <hisamhumaidi@gmail.com>
2021-11-29 17:34:36 +0700

    edit index dan style

--- js/script.js
+++ js/script.js
@@ -1,40 +1,59 @@
 // Link active
-let navbar = document.querySelector(".navbar");
+let navbarToggle = document.querySelector(".navbar");
 let section = document.querySelectorAll("section");
 let navlink = document.querySelectorAll("header nav ul li a");
+let searchBtn = document.querySelector("#search-btn");
+let searchBar = document.querySelector(".search-container");
+let menuToggle = document.querySelector("#menu-bars");
+let formClose = document.querySelector("#form-close");
 
-navbar.onclick = function () {
-  navbar.classList.toggle("active");
-};
+menuToggle.addEventListener("click", () => {
+  navbarToggle.classList.toggle("active");
+  menuToggle.classList.toggle("fa-times");
+});
 
-window.onscroll = () => {
-  // menu.classList.remove("fa-times");
-  navbar.classList.remove("active");
+searchBtn.addEventListener("click", function () {
+  searchBtn.classList.toggle("fa-times");
+  searchBar.classList.toggle("active");
+});
+// window.onscroll = () => {
+//   navbar.classList.remove("active");
+//   searchBtn.classList.remove("active");
+//   searchBar.classList.remove("fa-times");
+//   menuToggle.classList.remove("fa-times");
 
-  section.forEach((sec) => {
-    let top = window.scrollY;
-    let height = sec.offsetHeight;
-    let offset = sec.offsetTop - 150;
-    let id = sec.getAttribute("id");
+//   section.forEach((sec) => {
+//     let top = window.scrollY;
+//     let height = sec.offsetHeight;
+//     let offset = sec.offsetTop - 150;
+//     let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navlink.forEach((links) => {
-        links.classList.remove("active");
-        document.querySelector("header .navbar ul li a[href*=" + id + "]").classList.add("active");
-      });
-    }
-  });
-};
+//     if (top >= offset && top < offset + height) {
+//       navlink.forEach((links) => {
+//         links.classList.remove("active");
+//         document.querySelector("header .navbar ul li a[href*=" + id + "]").classList.add("active");
+//       });
+//     }
+//   });
+// };
+
+// formClose.addEventListener("click", function () {
+//   formClose.classList.remove("active");
+// });
+
+// navbar.onclick = function () {
+//   navbar.classList.toggle("active");
+// };
 
 // Sticky Navbar
 let navbarSticky = document.getElementById("nav");
 
 window.onscroll = function () {
   if (window.pageYOffset > 50) {
-    navbarSticky.style.background = "#bdbdbd";
+    navbarSticky.style.background = "#fff";
     navbarSticky.style.boxShadow = "0px 5px 2px blue lighten-4";
   } else {
-    navbarSticky.style.background = "transparent";
+    navbarSticky.style.background = "rgb(238, 237, 237)";
     navbarSticky.style.boxShadow = "none";
   }
 };
